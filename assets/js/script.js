5fc2b0eba20db7097fea90afe73df1c2b4752d14
GunnySensei <gunnerwagoner@hotmail.com>
2021-11-29 09:39:48 -0600

    listing and creating divs in javascript on eventhandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
-var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-
 //finds the form element and assigns formEl
 var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
+    //create variable that accesses the input of task-name [] = selects an HTML element
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskNameInput);
+
+    //creates the list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to the list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to the div
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
