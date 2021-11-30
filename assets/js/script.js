61966234daff1fbfb45545437eef69a6280db0f7
Gunner Wagoner <gunnerwagoner@gmail.com>
2021-11-30 09:13:14 -0600

    working in isEdit and finalizing editing process

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,37 @@ var taskFormHandler = function(event) {
         return false;
     }
 
+    var isEdit = formEl.hasAttribute("data-task-id");
+    //has data attribute so get task id and call function to complete editing process
+    if(isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+    //no data attribute, so create object as normal and pass to createTaskEl
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+
+        createTaskEl(taskDataObj);
+    }
+
+    var completeEditTask = function(taskName, taskType, taskId) {
+        // find the matching task list item
+        var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+        //set new values
+        taskSelected.querySelector("h3.task-name").textContent = taskName;
+        taskSelected.querySelector("span.task-type").textContent = taskType;
+
+        alert("Task Updated!");
+
+        //reset form 
+        formEl.removeAttribute("data-task-id");
+        document.querySelector("#save-task").textContent = "Add Task";
+    }
+
     //package data as an object
     var taskDataObj = {
         name: taskNameInput,
